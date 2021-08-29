03cc643953e8556734adb30c07fdafc755c5fb70
Ian Dela Cruz <iankencruz@gmail.com>
2021-08-29 15:38:51 +1000

    only 28 items problem

--- webscrape.py
+++ webscrape.py
@@ -1,30 +1,44 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
-
-my_url = 'https://www.digidirect.com.au/cameras/mirrorless-cameras/filters/brand/fujifilm-2'
+my_url = 'https://www.digidirect.com.au/lenses/mirrorless-lenses'
 
 uClient = uReq(my_url)
 page_html = uClient.read()
 uClient.close()
+
 # parse as a bs4 object
 page_soup = soup(page_html, 'html.parser')
 
 #grabs each product
-containers = page_soup.find_all("div", {"class": "product-item-info -grid"})
-container = containers[0]
+#! NOT FINDING ALL ITEMS
+items = page_soup.find_all("li", {'class': 'item product product-item'})
 
-# Get product name
-for container in containers:
-    product_name = container.strong.a['title']
-    print(product_name)
-    oldprice = oldprice = container.findAll("div", {"class":"current-price-wrapper"})
-    oldprice[0].text
-    print(oldprice[0].text)
-    
+ 
+print (len(items))
 
 
+# # Get product name
+# for container in items:
+#     try:
+#         product_name = container.strong.a['title']
+#         print("Product: " + product_name)
+#         priceList = container.find_all("span", {"class":"price"})
+
+#         oldprice = priceList[0].text
+#         newprice = priceList[1].text
+#         print("New Price: " + newprice)
+#     except:
+#         print("NO SALE PRICE FOUND!")
+#         # oldprice = priceList[0].text
+#         print("Top Price: " + oldprice)
+#     else:
+#         # oldprice = priceList[0].text
+#         print("Top Price: " + oldprice)
+
+
+#     print("\n")
 
 
 
-#TODO: write a try and except block for items that dont have special discount price
+#TODO: write a try, except, else block for items that dont have special discount price

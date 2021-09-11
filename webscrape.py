a06d279c420335da53219cce61fae258a7fc3907
Ian Dela Cruz <iankencruz@gmail.com>
2021-09-12 09:15:28 +1000

    trying to add new features

--- webscrape.py
+++ webscrape.py
@@ -1,33 +1,93 @@
 import time
 from bs4 import BeautifulSoup as soup
 from selenium import webdriver
+import pandas as pd
 
-my_url = 'https://www.digidirect.com.au/cameras/mirrorless-cameras'
+_digi_url = 'https://www.digidirect.com.au/cameras/mirrorless-cameras'
+_georges_url = 'https://www.georges.com.au/digital-cameras/mirrorless-cameras.html'
 
-#TODO Use request instead of selenium to load the rest of the items
 #TODO Scrape Georges camera aswell
 #TODO refactor in to a function to reuse scraping code
+
+
+def ScrollPage(driver, scrollPauseTime, screenHeight, iter):
+    while True:
+    # scroll one screen height each time
+        driver.execute_script("window.scrollTo(0, {scrollHeight}*{i});".format(scrollHeight=screenHeight, i=iter))  
+        iter += 1
+        time.sleep(scrollPauseTime)
+
+    # update scroll height each time after scrolled, as the scroll height can change after we scrolled the page
+        scrollHeight = driver.execute_script("return document.body.scrollHeight;")  
+    
+    # Break the loop when the height we need to scroll to is larger than the total scroll height
+        if (screenHeight) * iter > scrollHeight:
+            print ("Finished Scrolling!")
+            break 
+    
+        print("Scrolling!")
+
+
+
+
+def LoadPage(sel_driver, dest):        
+    # Load webpage
+    sel_driver.get(dest)    
+
+    # Wait before scrolling
+    time.sleep(0.3)
+
+  
+
+def ParseHTML(sel_driver, html_tag : str, id : str, id_name : str):
+    # parse as a bs4 object
+    page_soup = soup(sel_driver.page_source, 'html.parser')
+
+    #grabs each product
+    result_items = page_soup.findAll(html_tag, {id: id_name}) 
+
+    return result_items
 
 
 
 # Load webdriver
 driver = webdriver.Chrome()
 
-# Load webpage
-driver.get(my_url)
-
-# Wait before scrolling
-time.sleep(0.3)
-
-# Get the body element to scroll
-elem = driver.find_element_by_tag_name("body")
 
 # Pause time in between scrolls
-scrollPauseTime = .5 
+_scrollPauseTime = .5 
 
 # get the screen height of the web
 screenHeight = driver.execute_script("return window.screen.height;")   
 i = 1
+
+
+
+
+# Load The desired webpage
+LoadPage(driver, _digi_url)
+
+# function refactor
+ScrollPage(driver, _scrollPauseTime, screenHeight, i)
+
+
+
+
+
+
+
+items = ParseHTML(driver, 'li','class','item product product-item')
+
+
+
+
+
+
+
+top_price = ''
+new_price = ''
+
+
 
 # Write to file setup
 filename = 'product_cameras.csv'
@@ -35,71 +95,56 @@ file = open(filename, 'w')
 
 
 #* WRITE THE HEADERS OF THE CSV FILE
-headers = 'Product Name, Top Price, Sale Price\n'
+headers = 'Product Name, Brand,Top Price, Sale Price \n'
 file.write(headers)
 
 
+
+
+
+def GetProductInfo(bs4_item, html : str, id : str, id_name : str):
+    product_name = bs4_item.strong.a['title'] 
+
+
+
+
+
 #* Filter by brand
-search_target = "Fujifilm"
-
-
-while True:
-    # scroll one screen height each time
-    driver.execute_script("window.scrollTo(0, {scrollHeight}*{i});".format(scrollHeight=screenHeight, i=i))  
-    i += 1
-    time.sleep(scrollPauseTime)
-
-    # update scroll height each time after scrolled, as the scroll height can change after we scrolled the page
-    scrollHeight = driver.execute_script("return document.body.scrollHeight;")  
-    
-    # Break the loop when the height we need to scroll to is larger than the total scroll height
-    if (screenHeight) * i > scrollHeight:
-        break 
-    
-    print("Scrolling!")
-
-print ("Finished Scrolling!")
-
-
-# parse as a bs4 object
-page_soup = soup(driver.page_source, 'html.parser')
-
-#grabs each product
-items = page_soup.findAll("li", {'class': 'item product product-item'})
-
-
-oldprice = ''
-newprice = ''
-
+search_target = ["Fujifilm", "Sony", "Canon"]
 
 # Get product name
 for container in items:
-    if ( search_target in container.strong.a['title'] ):
+    for i in range(len(search_target)):
+        if (search_target[i] in container.strong.a['title'] ):
 
-        try:
-            #TODO sort product name to match sorted price
-            product_name = container.strong.a['title'] 
-            print("Product: " + product_name)
-            priceList = container.find_all("span", {"class":"price"})
+            try:
+                product_name = container.strong.a['title'] 
+                print("Product: " + product_name)
 
+                priceList = container.find_all("span", {"class":"price"})
 
-            # Grab old and new price from span class
-            # Format text to remove comma
-            oldprice = priceList[0].text.replace(',', '')
-            newprice = priceList[1].text.replace(',', '')
-
-            print("New Price: " + newprice)
+                brand_name = search_target[i]
 
 
+                # Grab old and new price from span class
+                # Format text to remove comma
+                top_price = priceList[0].text.replace(',', '')
+                new_price = priceList[1].text.replace(',', '')
 
-        except:
-            print("NO SALE PRICE FOUND!")
-            print("Top Price: " + oldprice)
-        else:
-            print("Top Price: " + oldprice)
+                print("New Price: " + new_price)
 
-        file.write(product_name + ',' +  oldprice + ',' +  newprice + '\n')
-        print("\n")
+
+
+
+            except:
+                print("NO SALE PRICE FOUND!")
+                print("Top Price: " + top_price)
+            else:
+                print("Top Price: " + top_price)
+
+            # file.write(product_name + ',' +  oldprice + ',' +  newprice + '\n')
+            file.write(f'{product_name},{brand_name},{top_price},{new_price} \n')
+            print("\n")
 
 
 print("Total items found: " + str(len(items)) + '\n')
@@ -107,4 +152,5 @@ print("Total items found: " + str(len(items)) + '\n')
 file.close()
 
 driver.quit()
+
 

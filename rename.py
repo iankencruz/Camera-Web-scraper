460a61acd9233307c18adaf71bd7853b8e90d739
Ian Dela Cruz <iankencruz@gmail.com>
2021-09-21 03:03:52 +1000

    Both rename and replace working

--- rename.py
+++ rename.py
@@ -12,70 +12,68 @@ class ReName:
         print(f'CWD: {self.path}') 
 
 
-    def walk_dir(self, path=None):
-        ''' 
-        Walk Directory of Path. 
-             Params:  
-                path ( str ) : Target path you want to search(Default is cwd)
-        '''
-        if path is None:
-            path = self.path            
-
-        file_list = []
-
-        for root, folders, files in os.walk(path):
-            for file in files:
-                # file_name, file_ext = os.path.splitext(file)
-                # print(f"Files: {file}")
-                file_list.append(file)
-
-        return file_list
-        
-
     def batch_rename(self, name, path=None, ):
         
         ''' 
         Walk Directory of Path. 
              Params:  
+                name ( str ) : Target name to replace old name
                 path ( str ) : Target path you want to search(Default is cwd)
         '''
         
-        iter_amnt = 1
-
+        # Function operation checks
         if path is None:
-            path = self.path            
+            path = self.path   
 
 
-        for root, folders, files in os.walk(path):
 
-            
-            for file in files:
+        iter_amnt = 1
                 
 
-
+        for root, folders, files in os.walk(path):
+            for file in files:
                 file_name, file_ext = os.path.splitext(file)
-                # print(f"Files: {file}")
 
                 try:
                     old_name = f'{path}\{file_name}{file_ext}'
                     print(f"Old Name: {old_name}\n")
                     new_name = f'{path}\{name}({iter_amnt}){file_ext}'
                     print(f"New Name: {new_name}\n")
-
                     # os.rename(old_name, new_name)
 
-                    iter_amnt += 1
-                except FileNotFoundError:
-                    print("Error: Cannot find files")
                     
-            print(len(files))
 
-        # print(f_list)
-        print ("batch rename")
-        
+                    iter_amnt += 1
+                except IndexError:
+                    print("Index out of range")
+                            
+    def batch_replace(self, src_name='', target_name='', path=None):
 
-    def batch_replace():
-        pass
+
+
+        # Function operation checks
+        if path is None:
+            path = self.path   
+
+                
+
+        for root, folders, files in os.walk(path):
+            for file in files:
+                if(src_name in file):
+                    file_name, file_ext = os.path.splitext(file)
+                    f_tag, f_title = file_name.split(src_name, 1)
+
+                    try:
+                        old_name = f'{path}\{file_name}{file_ext}'
+                        new_name = f'{path}\{target_name}{f_title}{file_ext}'
+                        print(f'Old Name = {old_name}')
+                        print(f'New Name = {new_name}')
+                        # os.rename(old_name, new_name)
+                        
+
+                        
+                    except IndexError:
+                        print("Index out of range")
 
 
 
@@ -87,29 +85,22 @@ class ReName:
 
 def main():
 
+    # Setup files and dirs
+    location =r"E:\Anime\Bleach\dev"
+
     rn = ReName()
     rn.get_info()
-    rn.batch_rename("kenny", path=r"E:\Anime\Bleach\[Judas] Bleach 001-055 [BD 1080p][HEVC x265 10bit]")
-
-    # location = f'C:\\Users\\PC\AppData\Local\CaptureOne\Styles50\IKM'
-
-    # for root, folder, files in os.walk(location):
-    #     for name in files:
-    #         file_name, file_ext = os.path.splitext(name)
-    #         f_title = file_name.split("_", 1)
-
-    #         try:
-    #             old_name =  f'{location}\{file_name}{file_ext}'
-    #             new_name = f'{location}\{f_title[1]}{file_ext}'
-    #             os.rename(old_name, new_name)
-    #             print(f'old name :{old_name}')
-    #             print(f'new name :{new_name}')
-        
-    #         except IndexError:
-    #             print("Index out of range")
+    
 
 
+    # Select type of renaming process
 
+
+    #*  Confirm inputs
+
+    
+    # rn.batch_rename("kenny", location)
+    # rn.batch_replace("-" , 'Bleach -',path=location)
 
 if __name__ == "__main__":
     main()
